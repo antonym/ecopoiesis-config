f783321d4efed10458c9d3b6657c95ae0b3662b3
Antony Messerli <amesserl@rackspace.com>
2017-03-02 17:11:26 -0600

    Rename checkout directory to avoid collision

--- firstboot/ecopoiesis-firstboot.sh
+++ firstboot/ecopoiesis-firstboot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ansible-pull -C master -d /opt/ecopoiesis -i localhost -U https://github.com/antonym/ecopoiesis.git --accept-host-key --purge
+ansible-pull -C master -d /opt/ecopoiesis-deploy -i localhost -U https://github.com/antonym/ecopoiesis.git --accept-host-key --purge
 
 systemctl disable ecopoiesis-firstboot.service
 
